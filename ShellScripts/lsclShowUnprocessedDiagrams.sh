b7f10f235ba401606b238ad63b5cfd0f95cfc6c7
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2024-03-06 11:11:40 +0100

    Add script for showing unprocessed diagrams at the given stage.

---
+++ ShellScripts/lsclShowUnprocessedDiagrams.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# This file is a part of LoopScalla, a framework for loop calculations
+# Loopscalla is covered by the GNU General Public License 3.
+# Copyright (C) 2019-2023 Vladyslav Shtabovenko
+
+if [[ $# -lt 5 ]] ; then
+    echo 'You must specify the project, the process name, the model, the number of the loops and the stage!'
+    exit 0
+fi
+
+################################################################################
+# Stop if any of the commands fails
+set -e
+
+lsclScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+lsclRepoDir="$(dirname $lsclScriptDir)"
+
+if [ -z "${lsclEnvSourced}" ]; then
+  . "$lsclRepoDir"/environment.sh
+fi
+
+################################################################################
+
+lsclProjectName="$1"
+lsclProcessName="$2"
+lsclModelName="$3"
+lsclNLoops="$4"
+lsclStage="$5"
+
+lsclAllReductions=()
+lsclCompletedReductions=()
+readarray -d '' lsclAllDiagramsRaw < <(find $lsclRepoDir/Projects/$lsclProjectName/Diagrams/Input/$lsclProcessName/$lsclModelName/$lsclNLoops -type f -name "dia*L$lsclNLoops.frm" -print0 | sort -V);
+
+IFS=$'\n' lsclAllDiagrams=($(sort -V <<<"${lsclAllDiagramsRaw[*]}"))
+unset IFS
+
+echo Unprocessed diagrams:
+for i in "${lsclAllDiagrams[@]}"; do
+	
+  lsclDiaName=$(basename ${i} .frm)  
+  
+  if [ ! -f $lsclRepoDir/Projects/$lsclProjectName/Diagrams/Output/$lsclProcessName/$lsclModelName/$lsclNLoops/Stage${lsclStage}/stage${lsclStage}_$lsclDiaName.res ]; then
+   echo $lsclDiaName
+  fi
+done
